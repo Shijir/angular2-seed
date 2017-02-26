cf3e110220a5243b1afd113073850f1297fdd739
stsogoo <stsogoo@vmware.com>
2017-02-26 02:08:58 -0800

    Testing AoT with private properties

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,8 +2,9 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'home',
-  styleUrls: ['./home.component.css'],
-  templateUrl: './home.component.html'
+  styleUrls: ['home.component.css'],
+  templateUrl: 'home.component.html'
 })
 export class HomeComponent {
+  private title = 'Home';
 }
